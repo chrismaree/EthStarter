8d3075a71b65f493b8d241632d234dba809ba50d
SoIidarity <christopher.maree@gmail.com>
2018-08-25 13:03:46 +0200

    Added Rinkeby testnet addresses

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,17 @@ var CampaignManagerContract = artifacts.require("CampaignManager");
 var ProxyContract = artifacts.require("Proxy")
 
 module.exports = function (deployer) {
-    // deployer.deploy(CampaignManager);
+    // First, we deploy the campaign manager contract
     deployer.deploy(CampaignManagerContract).then((value) => {
+        // extract the address from the deployed contract
         let deployedCampaignManagerAddress = value.address;
+        // Then, deploy the Proxy Contract
         deployer.deploy(ProxyContract).then((value) => {
+            // Create an instance of the proxy contract so we can assign the campaignManager to it
             let proxyInstance = ProxyContract.at(value.address)
+            // set the instance of the campaign manager to the proxy
             proxyInstance.upgradeTo(deployedCampaignManagerAddress)
             console.log("Proxy contract set to deployed CampaignManager!")
         })
     });
-    // console.log(address)
 };
--- truffle.js
+++ truffle.js
@@ -1,9 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "skin moment dizzy boost bitter network chaos pink horror credit that polar";
+
 module.exports = {
-  networks: {
+    networks: {
       development: {
         host: 'localhost',
         port: 8545,
         network_id: '*' // Match any network id
+      },
+      rinkeby: {
+        provider: function () {
+          return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/79738d10e34046b9abe619d5fdb53a3c");
+            },
+            network_id: 1
+        }
       }
     }
-};
